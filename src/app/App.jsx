e3fbdb6857ee45c340dc501ac9e4658ef46d9f02
yarield <derexkcg@gmail.com>
2026-04-22 14:54:59 -0600

    theme completado

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,29 @@
-import { TaskBoard } from "../features/tasks/components/TaskBoard"
 import { HTML_TAGS } from "../shared/constants/html-tags.constants"
+import { TaskBoard } from "../features/tasks/components/TaskBoard"
+import { ThemeProvider } from "../features/theme/context/ThemeProvider"
+import { ThemeToggle } from "../features/theme/components/ThemeToggle"
+import { useTheme } from "../features/theme/hooks/useTheme"
+import "./styles/index.css"
 
-const TitleTag = HTML_TAGS.H1
+function AppContent() {
+  const MainTag = HTML_TAGS.MAIN
+  const TitleTag = HTML_TAGS.H1
+  const { theme } = useTheme()
+  return (
+    <MainTag className={theme}>
+      <TitleTag>TaskBoard Hooks Lab</TitleTag>
+      <ThemeToggle />
+      <TaskBoard />
+    </MainTag>
+  )
+}
 
 function App() {
-  return <TitleTag>TaskBoard Hooks Lab
-    <TaskBoard />
-  </TitleTag>
+  return (
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
+  )
 }
 
 export default App
